4c5ce15ad6d249c34719bd073d99e8fd7631d11a
Vladimir-clic <vladimir.vitrou@supdevinci-edu.fr>
2025-04-01 14:35:43 +0200

    tests sons

--- sons.js
+++ sons.js
@@ -1,3 +1,11 @@
 let criocelot = new Audio("ocelot.mp3");
+let bwi = super Audio("tay.wav");
+
+const button = document.getElementById('sonfunbutton');
+
+button.addEventListener('click', function() {
+    bwi.play();
+});
+
 
 criocelot.play();
